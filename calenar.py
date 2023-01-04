daab6374ffe7717bd0acb117bc73789d348ae56a
Deniss Hohlov <deniss.hohlov@gmail.com>
2023-01-04 15:01:44 +0200

    calendar working

--- calenar.py
+++ calenar.py
@@ -9,17 +9,20 @@ root = Tk()
 root.geometry("400x400")
 
 # Add Calendar
-cal = Calendar(root, selectmode='day',
-               year=2020, month=5,
-               day=22)
+cal = Calendar(root, selectmode='day', year=2022, month=1, day=1)
 
 cal.pack(pady=20)
 
-dateq = cal.get_date()
-
 
 def grad_date():
-    date.config(text="Selected Date is: " + cal.get_date())
+    user_date = cal.get_date().split('/')
+    if int(user_date[0]) >= 10:
+        date1 = '20' + user_date[2] + user_date[1] + user_date[0]
+    else:
+        date1 = '20' + user_date[2] + user_date[1] + '0' + user_date[0]
+    print(date1)
+
+    # date.config(text="Selected Date is: " + date1)
 
 
 # Add Button and Label
